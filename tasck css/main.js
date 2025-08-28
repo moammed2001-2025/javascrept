ebc07c311cc68ccd0eb8043282fbe5570cfd7ebf
yourName <email@gmail.com>
2025-08-28 14:07:35 +0300

    tasck css

---
+++ tasck css/main.js
@@ -0,0 +1,14 @@
+// let x = 10 ;
+// let y = 5 ;
+// let z = 15 ;
+// if(x > y){
+//     if( z < x)
+//     console.log(x)
+// else if(z > x){
+//     console.log(z)
+// }
+// else{
+//     console.log(y)
+// }
+// }
+ 
